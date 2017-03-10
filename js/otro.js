f4fb8fa0d542673d22489f86e8c4aba1ad6e765e
Luis <luispalominot@hotmail.com>
2017-03-10 14:37:07 +0000

    Strategic Objective final

--- js/otro.js
+++ js/otro.js
@@ -1,1 +1,21 @@
-$(document).foundation()
+var flag=true;
+document.getElementById("1").style.display = "inline";
+document.getElementsByClassName("float-right")[0].addEventListener("click",ven)
+document.getElementsByClassName("float-left")[0].addEventListener("click",ven)
+
+function ven(){
+    if (flag){
+        show("2","1");
+        flag=false;
+    }
+    else{
+        show("1","2");
+        flag=true;
+    }
+    
+}
+
+function show(ID,id){
+    document.getElementById(ID).style.display = "none";
+    document.getElementById(id).style.display = "initial";
+}
