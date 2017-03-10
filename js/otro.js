a66645ff329a610f42587cef6cdf17930c9bc672
Luis <luispalominot@hotmail.com>
2017-03-10 15:42:30 +0000

    Actualizacion Strategic Objective

--- js/otro.js
+++ js/otro.js
@@ -1,21 +1,29 @@
-var flag=true;
-document.getElementById("1").style.display = "inline";
-document.getElementsByClassName("float-right")[0].addEventListener("click",ven)
-document.getElementsByClassName("float-left")[0].addEventListener("click",ven)
 
-function ven(){
-    if (flag){
-        show("2","1");
-        flag=false;
+document.getElementById("0").style.display = "inline";
+document.getElementsByClassName("tasks")[0].addEventListener("click",but)
+document.getElementsByClassName("steps1")[0].addEventListener("click",but)
+document.getElementsByClassName("steps2")[0].addEventListener("click",but)
+document.getElementsByClassName("steps3")[0].addEventListener("click",but)
+
+function but(e){
+    console.log(this.className);
+    var f;
+    if(this.className=="button tasks"){
+        f="0";
     }
-    else{
-        show("1","2");
-        flag=true;
+    else if(this.className=="button steps1"){
+        f="1";
     }
+    else if(this.className=="button steps2"){
+        f="2";
+    }
+    else if(this.className=="button steps3"){
+        f="3";
+    }
+    for(var i=0;i<4;i++){
+    document.getElementsByClassName("esconder")[i].style.display = "none";
+    }
+    document.getElementById(f).style.display = "initial";
     
 }
 
-function show(ID,id){
-    document.getElementById(ID).style.display = "none";
-    document.getElementById(id).style.display = "initial";
-}
