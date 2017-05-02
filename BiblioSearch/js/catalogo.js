82e52da970605e4e4cf7584ec1c9fc78b7c0de04
Luis <luispalominot@hotmail.com>
2017-05-02 13:55:40 +0000

    Corrected minor errors

--- BiblioSearch/js/catalogo.js
+++ BiblioSearch/js/catalogo.js
@@ -1,1 +1,18 @@
 $(document).foundation()
+
+
+// The next lines of code make the navigation var fixed when scrolling through the page
+var firstTop = $('.nav-container').offset().top + 1;
+
+
+$(window).scroll(function(){
+ 		var pTop = $('body').scrollTop();
+
+    	if(pTop >= firstTop){
+    	    $('.nav-container').css({'position':'fixed', 'width':'100%', 'top':0});
+    	    $('.top').css({'margin-top': $('.nav-container').height() + 'px'});
+    	}else{
+    	    $('.nav-container').css({'position':'relative', 'width':'100%'});
+    	    $('.top').css({'margin-top':0});
+    	}
+});
